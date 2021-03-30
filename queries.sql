6a470a0d99741269181590307f30951898e81e0c
kamjad1 <73908177+kamjad1@users.noreply.github.com>
2021-03-30 00:19:33 -0500

    added sql queries

--- queries.sql
+++ queries.sql
@@ -1,7 +1,21 @@
 ## Part 1: Test it with SQL
+ CREATE TABLE job (
+    id INT,
+    name VARCHAR(255),
+    employer_id INT
+    );
 
 ## Part 2: Test it with SQL
+SELECT name
+FROM employer
+WHERE (location = "St.Louis")
+
 
 ## Part 3: Test it with SQL
+DROP TABLE job;
 
 ## Part 4: Test it with SQL
+SELECT name, description
+FROM skill
+FROM skill WHERE id IN (SELECT skills_id FROM job_skills WHERE jobs_id is not null)
+ORDER BY name ASC
